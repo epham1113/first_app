df1475e0216f72b5352f56eb91410d19cd4b907f
U-OKHEART\epham <epham@OHH-epham.ocaheart.okheart.com>
2011-02-20 21:30:37 -0600

    initial command

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'fec76f32b9de835e1e13d85558c7c8bab573824a74737d3653c727e6364054fcc87078468f68874f4a67dbdd2c5687adf396f3d0405108cd64be0c97033c26ca'
